8734878a452aeed0e0e80178550b56c3528ade15
nax3t <iantskon@gmail.com>
2014-12-18 17:21:20 -0800

    Update files for heroku

--- db.js
+++ db.js
@@ -2,15 +2,12 @@
 var db = {};
 var pg = require('pg');
 
-db.config = {
-  database: "books2",
-  port: 5432,
-  host: "localhost",
-  user: "postgres"
-};
+db.config = {}
 
 db.connect = function(runAfterConnecting) {
-  pg.connect(db.config, function(err, client, done){
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if (err) {
       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
     }
